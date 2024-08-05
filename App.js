a161c2e4c6a8f6bf9c15389a16ecdff4f514050f
RJ123496 <rladhroya19@gmail.com>
2024-08-04 20:02:23 -0600

    this brings the screen and the context

--- App.js
+++ App.js
@@ -2,3 +2,13 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Overview from './screens/Overview';
+import BudgetGoals from './screens/BudgetGoals';
+import Layout from './screens/Layout';
+import Explore from './screens/Explore';
+import Index from './screens/Index';
+import Layout2 from './screens/Layout2';
+import AddTransaction from './screens/AddTransaction';
+import { TransactionsProvider } from './TransactionsContext';
+
+const Tab = createBottomTabNavigator();
