4ace6c4d8a6c981a2a7367b353e050c47896a9c9
RJ123496 <rladhroya19@gmail.com>
2024-08-04 18:38:22 -0600

    Inital setup for react

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
