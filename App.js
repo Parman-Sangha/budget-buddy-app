f9f327535b672891637147ad23deb252187a867f
RJ123496 <rladhroya19@gmail.com>
2024-08-05 20:30:27 -0600

    finsh app.js

--- App.js
+++ App.js
@@ -12,3 +12,22 @@ import AddTransaction from './screens/AddTransaction';
 import { TransactionsProvider } from './TransactionsContext';
 
 const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+      <TransactionsProvider>
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName="Overview">
+            <Tab.Screen name="Overview" component={Overview} />
+            <Tab.Screen name="BudgetGoals" component={BudgetGoals} />
+            <Tab.Screen name="Layout" component={Layout} />
+            <Tab.Screen name="Explore" component={Explore} />
+            <Tab.Screen name="Index" component={Index} />
+            <Tab.Screen name="Layout2" component={Layout2} />
+            <Tab.Screen name="AddTransaction" component={AddTransaction} />
+          </Tab.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </TransactionsProvider>
+    );
+  }
